e195fd8ee29863ceedbc54785787672205020b3f
Jahnavi5267june <84388382+Jahnavi5267june@users.noreply.github.com>
2021-06-30 16:37:59 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -1,10 +1,10 @@
 class Particle {
-    constructor(x, y) {
+    constructor(x, y,r) {
         var options = {
             restitution: 0.4,
             density:1.2
         }
-        this.r = 10;
+        this.r = r;
         this.body = Bodies.circle(x, y, this.r, options);
         this.color=color(random(0,255),random(0,255),random(0,255));
         World.add(world, this.body);
